b5a25b7bdd69b26eee7f5c02f921c7a8f28bf8b6
Midiy <polyk6859@gmail.com>
2025-02-06 17:25:26 +0300

    fix err

--- user.go
+++ user.go
@@ -2,8 +2,8 @@ package web_socket
 
 type User struct {
 	Id       int    `json:"-" db:"id"`
-	Name     string `json:"name" binding"required"`
-	Username string `json:"username" binding"required"`
-	Password string `json:"password" binding"required"`
-	Email    string `json:"email" binding"required"`
+	Name     string `json:"name" binding:"required"`
+	Username string `json:"username" binding:"required"`
+	Password string `json:"password" binding:"required"`
+	Email    string `json:"email" binding:"required"`
 }
